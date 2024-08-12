c63ff6acec05248f16f998ad141dffcc58d4a02e
madhuSudan27 <122591446+madhuSudan27@users.noreply.github.com>
2024-08-12 10:44:05 +0530

    working of require module , IIFE

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
-
-
 // const obj = require("./sum.js");
 
-// Object de-structuring on the fly 
+// Object de-structuring on the fly
 // const {calculateSum, multiply} = require("./sum.js")
 // import { calculateSum , multiply } from "./sum.js";
-
 
 // const {calculateSum} = require("./calculate/sum");
 // const {calculateMultiply} = require("./calculate/multiply");
 
+// lets import the folder
 
-// lets import the folder 
-
-const {calculateSum, calculateMultiply } = require("./calculate/index");
-
+const { calculateSum, calculateMultiply } = require("./calculate/index");
 
 // -------------------- EPISODE 3-----------------------------------------------------
-
 
 /* 
 var name = "Madhusudan anand";
@@ -41,17 +35,22 @@ console.log(global === globalThis); // true
 
  */
 
-
 // ---------------------------------------------EPISODE 4 ----------------------------------------
-
-
-
 
 const a = 10;
 const b = 20;
 
-calculateSum(a,b);
-calculateMultiply(a,b);
+calculateSum(a, b);
+calculateMultiply(a, b);
 
 
 
+// Uses module , export and other thing as parameter
+// from there we uses module.export ; 
+(function (module) {
+  // we can use same function and variables name which is declared at global scope
+  let a = 10;
+  const b = 20
+  console.log(a , b);
+  console.log("Node js Uses this IIFE to wrap the code before exporting the module or passing to the v8 Engine");
+})();
